3151a235733e383f457ac4d389953562924e0756
bozsikbeatrix <91243617+bozsikbeatrix@users.noreply.github.com>
2021-10-04 20:05:20 +0300

    Add files via upload

---
+++ _ln_k_o_/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "a=";
+    cin >> a;
+    cout << "b=";
+    cin >> b;
+    while(a!=b){
+        if(a>b){
+            a=a-b;
+        }else{
+            b=b-a;
+        }
+    }
+    cout << "a legnagyobb kozos oszto: " << a;
+    return 0;
+}
