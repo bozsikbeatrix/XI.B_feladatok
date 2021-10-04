db0e621892172059267d4e1983590c829b7bfd85
bozsikbeatrix <91243617+bozsikbeatrix@users.noreply.github.com>
2021-10-04 19:43:58 +0300

    Add files via upload

---
+++ _primvizsgalat_for/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int szam, oszto;
+    bool prim=true;
+    cout << "Kerem a szamot: ";
+    cin >> szam;
+    for(oszto=2;oszto<szam/2+1;oszto++){
+        if(szam%2==0){
+            prim=false;
+        }
+    }
+    if (prim){
+        cout << "a szam prim";
+    }else{
+        cout << "a szam nem prim";
+    }
+
+    return 0;
+}
