f8162d1f907c1c5bc49a6c14a36bca5cc73ab8f7
bozsikbeatrix <91243617+bozsikbeatrix@users.noreply.github.com>
2021-11-16 09:26:56 +0200

    string utolso szo elejere var2

--- 2021_11_16_lab_9/main.cpp
+++ 2021_11_16_lab_9/main.cpp
@@ -9,11 +9,10 @@ int main()
     cout << "Kerem a szoveget: ";
     cin >> str;
     int n= str.size();
-    char pohar=str[n-1];
-    for (int i=n-1; i>0; i--){
-        str[i]=str[i-1];
+    swap(str[0], str[n-1]);
+    for (int i=1; i<n-1; i++){
+        swap(str[i], str[n-1]);
     }
-    str[0]=pohar;
     cout << str;
     return 0;
 }
